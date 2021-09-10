8889cb9f1680c445fc821ce9e83256397f97d698
Sara Shaikh <65680776+Sara-Shaikh-21@users.noreply.github.com>
2021-09-11 00:30:15 +0530

    Update LinearSearch.cpp

--- LinearSearch.cpp
+++ LinearSearch.cpp
@@ -3,17 +3,33 @@ using namespace std;
 
 
 void linearSearch(int arr[],int size,int key){
+    int count=0;
     for(int i=0;i<4;i++){
+        
         if(arr[i]==key){
+           
             cout<<key<<" KEY IS FOUND at : "<<i+1<<" location"<<endl;
+        }else{
+             count++;
+        }
+        if(count==1){
+            cout<<"KEY IS NOT PRESENT IN ARRAY";
         }
     }
 }
 int main(){
-
-    int arr[4]={22,13,90,28};
-    int key=28;
-    int size=4;
-    linearSearch(arr,size,key);
+     int n,key;
+    cout<<"Enter the size of array ";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter array elements ";
+    for(int j=0;j<n;j++){
+        cin>>arr[j];
+    }
+    cout<<"Enter key to find ";
+    cin>>key;
+    
+    
+    linearSearch(arr,n,key);
     return 0;
 }
